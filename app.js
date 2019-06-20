0b0629e96de8fc6ee9a5cd30af812160f996b5f0
autumnfish <517729329@qq.com>
2019-06-20 21:58:47 +0800

    增加cors允许跨域

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 // 路由
 const userRouter = require(path.join(__dirname,'./routers/userRouter'))
 const heroRouter = require(path.join(__dirname,'./routers/heroRouter'))
@@ -7,6 +8,7 @@ const heroRouter = require(path.join(__dirname,'./routers/heroRouter'))
 const app = express()
 
 // 注册路由
+app.use(cors())
 app.use('/user',userRouter)
 app.use('/hero',heroRouter)
 app.use(express.static('static'))
